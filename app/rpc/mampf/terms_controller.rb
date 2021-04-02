763ae3f22620d0f0e4c1ef5cd337fee3993c25ec
Henrik Reinstädtler <reinstaedtler@stud.uni-heidelberg.de>
2021-04-02 12:43:32 +0200

    Cleanup terms controller for rpc

--- app/rpc/mampf/terms_controller.rb
+++ app/rpc/mampf/terms_controller.rb
@@ -8,16 +8,12 @@ module Mampf
       #
       def get_terms
         u = ::User.find(request.message.user_id)
-        p u
         terms = u.lectures.map{|l| l.term}.uniq.filter{|t| !t.nil?}
-        p terms
-        p terms.first.id
         tems_ = terms.to_a.map(&:serializable_hash).map do |t| 
             t["created_at"] = t["created_at"].to_time
             t["updated_at"]= t["updated_at"].to_time
-            t
+            t.except("submission_deletion_mail", "submission_deletion_reminder", "submissions_deleted_at")
         end.map{|ts| ::Mampf::Term.new(ts)}#id:ts.id,year: ts.year, season: ts.season, created_at:ts.created_at.to_time,updated_at:ts.updated_at.to_time, active:ts.active?)}
-        p "After",tems_
         return ::Mampf::TermsRequestResult.new(terms: tems_)
       rescue StandardError => _e
         p _e
